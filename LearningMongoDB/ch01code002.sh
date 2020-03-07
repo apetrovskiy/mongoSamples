05ae7cb7a06d1d4a57671244e75886c9e2871c63
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2020-03-07 15:38:51 +0300

    00001 (#1)

---
+++ LearningMongoDB/ch01code002.sh
@@ -0,0 +1,5 @@
+db.test.insert({"key":"value1","key2":"Val2","key3":"val3"})
+db.test.insert({"key":"value2","key2":"Val21","key3":"val31"})
+db.test.insert({"key":"value3","key2":"Val22","key3":"val33"})
+db.test.find()
+db.test.find().pretty()
