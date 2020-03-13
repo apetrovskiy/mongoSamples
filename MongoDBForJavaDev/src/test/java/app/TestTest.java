071c660e1547909cafcc88cc0af249819350adaf
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2020-03-13 11:13:20 +0300

    001

---
+++ MongoDBForJavaDev/src/test/java/app/TestTest.java
@@ -0,0 +1,12 @@
+package app;
+
+import org.junit.jupiter.api.Test;
+
+public class TestTest{
+    private static final String TEST01 = "test01";
+
+    @Test
+    void probe() {
+        System.out.println(TEST01);
+    }
+}
